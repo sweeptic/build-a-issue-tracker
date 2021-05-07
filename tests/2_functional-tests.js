e13346863322244d02b72d04a001570d9e75202c
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-07 11:08:32 +0200

    add some functional test cases

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,19 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite('Functional Tests', function () {
+  test('Create an issue with every field: POST request to /api/issues/{project} ', () => {});
+  test('Create an issue with only required fields: POST request to /api/issues/{project}', () => {});
+  test('Create an issue with missing required fields: POST request to /api/issues/{project}', () => {});
+  test('View issues on a project: GET request to /api/issues/{project}', () => {});
+  test('View issues on a project with one filter: GET request to /api/issues/{project}', () => {});
+  test('View issues on a project with multiple filters: GET request to /api/issues/{project}', () => {});
+  test('Update one field on an issue: PUT request to /api/issues/{project}', () => {});
+  test('Update multiple fields on an issue: PUT request to /api/issues/{project}', () => {});
+  test('Update an issue with missing _id: PUT request to /api/issues/{project}', () => {});
+  test('Update an issue with no fields to update: PUT request to /api/issues/{project}', () => {});
+  test('Update an issue with an invalid _id: PUT request to /api/issues/{project}', () => {});
+  test('Delete an issue: DELETE request to /api/issues/{project}', () => {});
+  test('Delete an issue with an invalid _id: DELETE request to /api/issues/{project}', () => {});
+  test('Delete an issue with missing _id: DELETE request to /api/issues/{project}', () => {});
 });
